6873c380e1cf00ec2611d1b71abc96f64d7ec7c0
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-10 23:39:37 +0530

    To do list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ToDoListComponent } from './to-do-list/to-do-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ToDoListComponent
   ],
   imports: [
     BrowserModule,
